468b21a579779e9b0ffd3c8e65e77401c21ff37a
Error4qu <madhav4412@gmail.com>
2026-04-29 01:24:11 +0530

    implement ci-app-test.prop

--- src/test/java/com/project/job_queue/JobQueueApplicationTests.java
+++ src/test/java/com/project/job_queue/JobQueueApplicationTests.java
@@ -2,8 +2,10 @@ package com.project.job_queue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JobQueueApplicationTests {
 
 	@Test
